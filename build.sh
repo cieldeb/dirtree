25cc51844bef02f5a9c1868ba0bee491a6fec9ee
cielWorkstation <outlook75012@gmail.com>
2026-01-24 12:55:20 +0100

    Upgraded build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
+echo "Formatting go files"
+go fmt
+
+echo "Building executable..."
 go build
 
+echo "Moving executable to ~/.local/bin"
 cp dirtree ~/.local/bin
 
 if [ $? -eq 0 ]; then
   echo "Moved new executable to ~/.local/bin"
   exit 0
 else
-  echo "Watermarker executable update failed"
+  echo "Watermarker executable copy failed"
 fi
