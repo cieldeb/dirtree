d335d10e71141d0d0368afb5e5c9cd8bce3303c2
ciel <outlook75012@gmail.com>
2026-04-09 22:49:25 -0300

    moved functions not linked to Tree structure to utils.go

---
+++ utils.go
@@ -0,0 +1,16 @@
+package main
+
+// pluralize adds suffixes to element names depending on the case
+func pluralize(base string, count int) string {
+	if base == "subdirector" {
+		if count == 1 {
+			return "subdirectory"
+		}
+		return "subdirectories"
+	}
+	// For "file"
+	if count == 1 {
+		return base
+	}
+	return base + "s"
+}
